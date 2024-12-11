93baee34e1af1087e84189b4dd342696dfc81f2d
Matthew S Cherney <matt66146@gmail.com>
2024-12-10 16:03:58 -0800

    day 9 part 1 done

--- Day 9/part1.py
+++ Day 9/part1.py
@@ -6,48 +6,53 @@ with open("input.txt") as input_file:
 
 data = []
 cur_id = 0
-new_data = []
+
 
 for i in range(0,len(input)):
     for j in range(0,int(input[i])):
         if i == 0 or i % 2 == 0:
             data.append(cur_id)
         else:
-            data.append(".")
+            data.append(-1)
     if i == 0 or i % 2 == 0:
         cur_id += 1
 
 
 #print(data)
-while data.count(".") > 0:
-    for j in range (len(data)-1,0,-1):
+
+
+countera = 0
+counterb = 0
+
+for i in range (len(data)-1,0,-1):
+    countera += 1
+    found = True
+    if data[i] != -1:
         found = False
-        if data[j] != ".":
-            free_space_found = False
-            found = True
-            for d in data:
-                if d == "." and not free_space_found:
-                    free_space_found = True
-                    new_data.append(data[j])
-                else:
-                    new_data.append(d)
-        else:
-            del data[-1]  
-        if found:
-            del new_data[-1]
-            #print(new_data)
-            data = new_data.copy()
-            new_data = []
-            break
+        for j in range(0,i):
+            counterb += 1
+            if data[j] == -1:
+                data[j] = data[i]
+                data[i] = -1
+                found = True
+                break
+    if not found:
+        break
+    #print(data)
+            
+                
     #break
 
 checksum = 0
 
 for i in range(0, len(data)):
     #print("{} * {}".format(data[i],i))
-    checksum += (data[i] * i)
+    if (data[i] != -1):
+        checksum += (data[i] * i)
     #print(checksum)
 
 
 print(data)
 print(checksum)
+print(countera)
+print(counterb)
