27dc2aa4b047f0d27b7f46d94884cdc383f68b3c
Matthew Cherney <matt@generaltechnique.com>
2025-10-19 00:22:17 -0700

    part 2

--- 2015/day_1/src/main.rs
+++ 2015/day_1/src/main.rs
@@ -2,17 +2,28 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
+    let part = 2; //Change to 1 to run part 1
+
     let mut floor = 0;
     let input_file = BufReader::new(File::open("./src/input").expect("Error opening file"));
-
+    let mut pos = 0;
     for line in input_file.lines() {
         for c in line.unwrap().chars() {
+            pos += 1;
             if c == '(' {
                 floor += 1;
             } else if c == ')' {
                 floor -= 1;
             } else {
                 println!("Warning! This should never happen!");
+            }
+
+            if part == 2 {
+                //Part 2
+                if floor == -1 {
+                    println!("Position {}", pos);
+                    return;
+                }
             }
         }
     }
