0d445ad30dc72e820ea62dcbea299b504151e26a
Matthew Cherney <matt@generaltechnique.com>
2025-10-19 16:06:36 -0700

    Switching back to simpler, faster solution

--- 2015/day_5/src/main.rs
+++ 2015/day_5/src/main.rs
@@ -1,13 +1,12 @@
+use core::num;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::time::Instant;
 
 fn main() {
     let input_file = File::open("./src/input").expect("Error Opening File");
     let input_file = BufReader::new(input_file);
     let mut num_good = 0;
     let mut num_good_part2 = 0;
-    let now = Instant::now();
     for l in input_file.lines() {
         let line = l.unwrap();
         //part 1
@@ -29,7 +28,7 @@ fn main() {
                 num_good_part2 += 1;
                 //println!("Good: {}", line);
             } else {
-                //println!("Check: {}", line);
+                println!("Check: {}", line);
             }
         } else {
             {
@@ -39,7 +38,6 @@ fn main() {
     }
     println!("Part 1 : {}", num_good);
     println!("Part 2 : {}", num_good_part2);
-    println!("Elapsed: {:.2?}", now.elapsed());
 }
 
 fn repeat_letter(string: &String) -> bool {
@@ -56,28 +54,14 @@ fn repeat_letter(string: &String) -> bool {
 fn check_pairs(string: &String) -> bool {
     let mut pairs: Vec<String> = vec![];
     let mut c = 0;
-    let mut prev: String = "".to_string();
 
     while c < string.len() - 1 {
-        if prev != string[c..c + 2].to_string() {
-            if pairs.contains(&string[c..c + 2].to_string()) {
-                return true;
-            }
-            pairs.push(string[c..c + 2].to_string());
-            prev = string[c..c + 2].to_string();
-        } else {
-            {
-                prev = "".to_string(); // If a back to back entry is found -- reset prev to allow for back to back to back edge cases
-                // EX: cccc
-                //The back to back cc should fail (to not reuse the middle c), but the next cc that doesnt reuse the original entry added to the list
-                // should succeed rather than fail as a duplicate
-            }
+        if string[c + 2..string.len()].contains(&string[c..c + 2].to_string()) {
+            return true;
         }
 
         c += 1;
     }
-
-    //println!("{}", pairs.join(","));
     return false;
 }
 
