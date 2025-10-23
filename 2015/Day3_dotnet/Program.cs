f08bf384d34a39ce9e6079c229a75817fc6d52de
Matthew S Cherney <matt66146@gmail.com>
2025-10-23 14:25:12 -0700

    day 3 dotnet

--- 2015/Day3_dotnet/Program.cs
+++ 2015/Day3_dotnet/Program.cs
@@ -1,2 +1,102 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string f = "";
+
+int x = 0;
+int y = 0;
+HashSet<(int, int)> houses = new();
+
+//Part2 ***********
+int santaX = 0;
+int santaY = 0;
+int roboX = 0;
+int roboY = 0;
+HashSet<(int, int)> housesPart2 = new();
+bool robo = false;
+//*****************
+
+try
+{
+    f = File.ReadAllText("input");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+    return;
+}
+
+foreach (char move in f)
+{
+    switch (move)
+    {
+        case '^':
+            y++;
+            houses.Add((x, y));
+
+            //part 2
+            if (!robo)
+            {
+                santaY++;
+                housesPart2.Add((santaX, santaY));
+            }
+            else
+            {
+                roboY++;
+                housesPart2.Add((roboX, roboY));
+            }
+            break;
+        case 'v':
+            y--;
+            houses.Add((x, y));
+
+            //part 2
+            if (!robo)
+            {
+                santaY--;
+                housesPart2.Add((santaX, santaY));
+            }
+            else
+            {
+                roboY--;
+                housesPart2.Add((roboX, roboY));
+            }
+            break;
+        case '<':
+            x--;
+            houses.Add((x, y));
+
+            //part 2
+            if (!robo)
+            {
+                santaX--;
+                housesPart2.Add((santaX, santaY));
+            }
+            else
+            {
+                roboX--;
+                housesPart2.Add((roboX, roboY));
+            }
+            break;
+        case '>':
+            x++;
+            houses.Add((x, y));
+
+            //part 2
+            if (!robo)
+            {
+                santaX++;
+                housesPart2.Add((santaX, santaY));
+            }
+            else
+            {
+                roboX++;
+                housesPart2.Add((roboX, roboY));
+            }
+            break;
+        default:
+            Console.WriteLine($"Invalid move character: {move}");
+            break;
+    }
+    robo = !robo;
+}
+
+Console.WriteLine($"Part 1 - Total houses visited: {houses.Count}");
+Console.WriteLine($"Part 2 - Total houses visited: {housesPart2.Count}");
