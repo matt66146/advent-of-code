796b65229a208ffed07fe998b62932dfdbbc3033
Matthew S Cherney <matt66146@gmail.com>
2025-10-23 14:35:02 -0700

    Day 4 dotnet

---
+++ 2015/Day4_Dotnet/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+
+string input = "yzbqklnj";
+int i = 0;
+
+bool part1Found = false;
+bool part2Found = false;
+
+using (var md5 = MD5.Create())
+{
+    while (!part1Found || !part2Found)
+    {
+        var bytes = System.Text.Encoding.ASCII.GetBytes(input + i.ToString());
+        var hashBytes = md5.ComputeHash(bytes);
+        var hashString = Convert.ToHexString(hashBytes);
+
+        if (!part1Found && hashString.StartsWith("00000"))
+        {
+            Console.WriteLine($"Part 1: {i}");
+            part1Found = true;
+        }
+
+        if (hashString.StartsWith("000000"))
+        {
+            Console.WriteLine($"Part 2: {i}");
+            part2Found = true;
+        }
+
+        i++;
+    }
+}
