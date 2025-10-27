c16ebcfb1e05b7aeb36ed859e8cd8b69430215f2
Matthew S Cherney <matt66146@gmail.com>
2025-10-26 21:01:34 -0700

    Day 9 part 1

---
+++ 2015/Day10/Program.cs
@@ -0,0 +1,38 @@
+﻿string input = "1321131112";
+
+
+
+for (int i = 0; i < 40; i++)
+{
+    Console.WriteLine(i);
+    input = RunProcess(input);
+}
+Console.WriteLine(input);
+Console.WriteLine(input.Length);
+
+
+
+string RunProcess(string input)
+{
+    string newInput = "";
+    for (int i = 0; i < input.Length;)
+    {
+        char c = input[i];
+        int num = 1;
+        int j = i + 1;
+
+        if (j < input.Length)
+        {
+            while (input[j] == c)
+            {
+                num++;
+                j++;
+            }
+        }
+        i += num;
+        //Console.WriteLine($"{num} {c}");
+        newInput += $"{num}{c}";
+    }
+
+    return newInput;
+}
