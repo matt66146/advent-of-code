68d0f8a28a58dbef1ecfe46f86191afc83a2ec80
Matthew Cherney <matt@generaltechnique.com>
2025-10-28 09:27:09 -0700

    Starting day 13

---
+++ 2015/Day13/Program.cs
@@ -0,0 +1,22 @@
+﻿var f = File.ReadAllLines("testInput");
+
+HashSet<string> guests = new();
+
+List<string> seats = new();
+
+
+foreach (var line in f)
+{
+    guests.Add(line.Split(' ').First());
+}
+
+foreach (var guest in guests)
+{
+    Console.WriteLine(guest);
+}
+
+
+class Guest(Dictionary<string, int> happiness)
+{
+    Dictionary<string, int> Happiness = happiness;
+}
