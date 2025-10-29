2500f0aba4c1b0d06b1c456b915cc0d7e354097f
Matthew Cherney <matt@generaltechnique.com>
2025-10-29 15:33:19 -0700

    Starting day 17

---
+++ 2015/Day17/Program.cs
@@ -0,0 +1,52 @@
+﻿var input = File.ReadAllLines("input");
+
+//Test input
+//input = ["20", "15", "10", "5", "5"];
+//*********
+
+List<int> containers = new();
+
+foreach (var line in input)
+{
+    containers.Add(Int32.Parse(line));
+}
+
+int max = 150;
+List<Dictionary<int, int>> combos = new();
+
+
+FindCombos(new Dictionary<int, int>(), containers);
+
+void FindCombos(Dictionary<int, int> combo, List<int> containers)
+{
+    Console.WriteLine("test");
+    if (combo.Values.Sum() == max)
+    {
+        combos.Add(combo);
+    }
+    else
+    {
+        foreach (var container in containers)
+        {
+            if ((combo.Values.Sum() + container) <= max)
+            {
+                Dictionary<int, int> currentCombo = new(combo);
+                currentCombo[container] = container;
+                List<int> containersLeft = new(containers);
+                containersLeft.Remove(container);
+                FindCombos(currentCombo, containersLeft);
+            }
+        }
+    }
+}
+
+Console.WriteLine($"Num Combos: {combos.Count()}");
+
+foreach (var c in combos)
+{
+    Console.WriteLine(string.Join(' ', c));
+}
+
+
+
+
