db361180c6cfdeb77c4b765b194c310d7887e106
Matthew S Cherney <matt66146@gmail.com>
2025-10-28 18:11:45 -0700

    Day 13 brute force

--- 2015/Day13/Program.cs
+++ 2015/Day13/Program.cs
@@ -1,22 +1,131 @@
-﻿var f = File.ReadAllLines("testInput");
+﻿var f = File.ReadAllLines("input");
 
-HashSet<string> guests = new();
+Dictionary<string, Dictionary<string, int>> guests = new();
 
-List<string> seats = new();
 
+List<string> guestNames = new();
 
 foreach (var line in f)
 {
-    guests.Add(line.Split(' ').First());
+    var parts = (line.Split(' '));
+    string guestName = parts[0];
+    string guestNameToCompare = parts[parts.Length - 1].Split(".")[0];
+    int happiness = 0;
+
+    happiness = Int32.Parse(parts.Where(p => Int32.TryParse(p, out happiness)).First());
+    if (parts.Contains("lose"))
+    {
+        happiness = happiness * -1;
+    }
+    //Console.WriteLine($"{guestName} {happiness} {guestNameToCompare}");
+    if (!guests.ContainsKey(guestName)) guests[guestName] = new Dictionary<string, int>();
+    guests[guestName][guestNameToCompare] = happiness;
+
+    if (!guestNames.Contains(guestName)) guestNames.Add(guestName);
 }
 
-foreach (var guest in guests)
+//Console.WriteLine(JsonSerializer.Serialize(guests));
+
+
+
+//Part 1
+var test = guestNames.Permute();
+
+List<int> happinessList = new();
+foreach (var order in test)
 {
-    Console.WriteLine(guest);
+    int totalHappiness = CalculateHappiness(order.ToList(), guests);
+    happinessList.Add(totalHappiness);
+    //Console.WriteLine(totalHappiness);
+}
+happinessList = happinessList.OrderByDescending(i => i).ToList();
+
+Console.WriteLine($"Part 1 - Total Happiness: {happinessList[0]}");
+
+//part 2
+guests["Matt"] = new();
+guestNames.Add("Matt");
+foreach (var guest in guestNames)
+{
+    guests["Matt"][guest] = 0;
+}
+foreach (var guest in guestNames)
+{
+    guests[guest]["Matt"] = 0;
 }
 
-
-class Guest(Dictionary<string, int> happiness)
+test = guestNames.Permute();
+happinessList = new();
+foreach (var order in test)
 {
-    Dictionary<string, int> Happiness = happiness;
+    //Console.WriteLine(JsonSerializer.Serialize(order));
+    int totalHappiness = CalculateHappiness(order.ToList(), guests);
+    happinessList.Add(totalHappiness);
+    //Console.WriteLine(totalHappiness);
+}
+happinessList = happinessList.OrderByDescending(i => i).ToList();
+
+Console.WriteLine($"Part 1 - Total Happiness: {happinessList[0]}");
+
+
+
+
+
+
+int CalculateHappiness(List<string> guestNames, Dictionary<string, Dictionary<string, int>> guests)
+{
+    int happiness = 0;
+
+    for (int i = 0; i < guestNames.Count; i++)
+    {
+        int left = i - 1;
+        if (left < 0) left = guestNames.Count - 1;
+        int right = i + 1;
+        if (right > guestNames.Count - 1) right = 0;
+
+        string guest = guestNames[i];
+        string leftGuest = guestNames[left];
+        string rightGuest = guestNames[right];
+
+        happiness += guests[guest][leftGuest];
+        happiness += guests[guest][rightGuest];
+
+    }
+
+    return happiness;
+}
+
+public static class Test
+{
+    public static IEnumerable<IEnumerable<T>> Permute<T>(this IEnumerable<T> sequence)
+    {
+        if (sequence == null)
+        {
+            yield break;
+        }
+
+        var list = sequence.ToList();
+
+        if (!list.Any())
+        {
+            yield return Enumerable.Empty<T>();
+        }
+        else
+        {
+            var startingElementIndex = 0;
+
+            foreach (var startingElement in list)
+            {
+                var index = startingElementIndex;
+                var remainingItems = list.Where((e, i) => i != index);
+
+                foreach (var permutationOfRemainder in remainingItems.Permute())
+                {
+                    yield return permutationOfRemainder.Prepend(startingElement);
+                }
+
+                startingElementIndex++;
+            }
+        }
+    }
 }
