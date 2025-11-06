68135e65306a1a93cbc22f69e6785439236dd908
Matthew S Cherney <matt66146@gmail.com>
2025-11-05 20:43:48 -0800

    start day21

--- 2015/Day21/Program.cs
+++ 2015/Day21/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Day21;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
