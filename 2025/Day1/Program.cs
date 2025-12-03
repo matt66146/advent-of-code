5f28609566e02f4e3e88569ea826dbef9b02a320
Matthew S Cherney <matt66146@gmail.com>
2025-12-03 15:13:17 -0800

    dumb answer works - smart answer dumb =(

--- 2025/Day1/Program.cs
+++ 2025/Day1/Program.cs
@@ -9,17 +9,30 @@ Console.WriteLine("*Real Input*");
 Calc(position, f);
 
 
-
-
 static void Calc(int position, string[] f)
 {
     int answer = 0;
     foreach (var line in f)
     {
         int change = Int32.Parse(line.Substring(1, line.Length - 1));
-
+        answer += (change - position) / 100;
+        if ((change - position) / 100 > 0)
+        {
+            Console.WriteLine("***");
+            Console.WriteLine(position);
+            Console.WriteLine(line);
+            Console.WriteLine((change - position) / 100);
+        }
         if (line[0] == 'L')
         {
+            if (position - change <= 0 && position != 0)
+            {
+                answer++;
+                if ((change - position) / 100 > 0)
+                {
+                    Console.WriteLine("+1");
+                }
+            }
             position = (position - change) % 100;
             if (position < 0)
             {
@@ -28,13 +41,47 @@ static void Calc(int position, string[] f)
         }
         else
         {
+            if (position + change >= 100)
+            {
+                answer++;
+                if ((change - position) / 100 > 0)
+                {
+                    Console.WriteLine("+1");
+                }
+            }
             position = (position + change) % 100;
         }
 
-        if (position == 0)
-        {
-            answer++;
-        }
+        //Console.WriteLine(answer);
+
     }
     Console.WriteLine($"Answer: {answer}");
+}
+
+
+static void BadCalcWTF(int position, string[] f)
+{
+    int answer = 0;
+
+    foreach (var line in f)
+    {
+        int change = Int32.Parse(line.Substring(1, line.Length - 1));
+
+
+        for (int i = 0; i < change; i++)
+        {
+            if (line[0] == 'L')
+            {
+                position--;
+                if (position == -1) position = 99;
+            }
+            else
+            {
+                position++;
+                if (position == 100) position = 0;
+            }
+            if (position == 0) answer++;
+        }
+    }
+    Console.WriteLine(answer);
 }
