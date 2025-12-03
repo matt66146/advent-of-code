ede70e9d4b72df665e25b217f39763bc13dee7bd
Matthew S Cherney <matt66146@gmail.com>
2025-12-03 14:19:24 -0800

    cleanup

--- 2025/Day1/Program.cs
+++ 2025/Day1/Program.cs
@@ -2,8 +2,10 @@
 
 var f = File.ReadAllLines("testinput.txt");
 
+Console.WriteLine("*Test Input*");
 Calc(position, f);
 f = File.ReadAllLines("input.txt");
+Console.WriteLine("*Real Input*");
 Calc(position, f);
 
 
@@ -12,7 +14,6 @@ Calc(position, f);
 static void Calc(int position, string[] f)
 {
     int answer = 0;
-    Console.WriteLine($"Start {position}");
     foreach (var line in f)
     {
         int change = Int32.Parse(line.Substring(1, line.Length - 1));
