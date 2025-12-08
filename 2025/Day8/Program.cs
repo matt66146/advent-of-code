3545bab264a2861b4cf2538ed7145c0508c6ceb5
Matthew S Cherney <matt66146@hotmail.com>
2025-12-07 22:21:38 -0800

    start

---
+++ 2025/Day8/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+
+
+
+
+Dictionary<int, List<JunctionBox>> circuits;
+
+class JunctionBox
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Z { get; set; }
+    public int Circuit { get; set; }
+}
